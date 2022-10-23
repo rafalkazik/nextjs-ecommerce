0843436a71357b03abff382a62e19cd26e8211d6
Rafał Kazik <kazik.raf@gmail.com>
2022-10-23 20:33:02 +0200

    create simple structure of page

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className='max-w-md mx-auto w-full '>
+      <nav className='bg-gray-300 px-4 py-2'>NAV</nav>
+    </header>
+  );
+};
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang='pl'>
       <Head />
-      <body>
+      <body className='bg-gray-100 antialiased'>
         <Main />
         <NextScript />
       </body>
