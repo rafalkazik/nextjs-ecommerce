2e860b9319ff3fe11e1c57a50cc9abd6b4323694
Rafał Kazik <kazik.raf@gmail.com>
2023-01-11 21:44:26 +0100

    add mailer api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,49 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiHandler } from 'next';
+import axios from 'axios';
 
-type Data = {
-  name: string
-}
+const handler: NextApiHandler = (req, res) => {
+  if (req.method !== 'POST') {
+    return res.setHeader('Allow', 'POST').status(405).json({});
+  }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+  const MAILERLITE_GROUP_ID = process.env.MAILERLITE_GROUP_ID;
+  const MAILERLITE_API_KEY = process.env.MAILERLITE_API_KEY;
+
+  if (!MAILERLITE_API_KEY || !MAILERLITE_GROUP_ID) {
+    return res
+      .status(500)
+      .json({ error: 'Nie podano zmiennych środowiskowych' });
+  }
+
+  const email = req.body.email;
+
+  if (typeof email !== 'string') {
+    return res.status(400).json({});
+  }
+
+  const options = {
+    method: 'POST',
+    url: `https://api.mailerlite.com/api/v2/groups/${MAILERLITE_GROUP_ID}/subscribers`,
+    headers: {
+      accept: 'application/json',
+      'X-MailerLite-ApiDocs': 'true',
+      'content-type': 'application/json',
+      'X-MailerLite-ApiKey': MAILERLITE_API_KEY,
+    },
+    data: {
+      email: email,
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+};
+
+export default handler;
